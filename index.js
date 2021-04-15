266f01876dbe5482742044ea57b30a0b1a0f9090
SamuelBarajas <80172255+SamuelBarajas@users.noreply.github.com>
2021-04-14 20:11:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -16,7 +16,7 @@ io.on('connection', (socket) => {
   });
 
 server.listen(port, () => {
-  console.log('listening on *:3000');
+  console.log('listening on *:5000');
 });
 
 
